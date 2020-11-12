c23a01875218e8a8705b4a0feac1ff24e25e3557
Harold <harold.goldensky@gmail.com>
2020-11-12 19:31:07 +0800

    Add files via upload

---
+++ assets/main js/validate.js
@@ -0,0 +1,24 @@
+function validate(){
+    var form = document.getElementById("form");
+    var email =document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email.match(pattern)){
+
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+       
+        
+    }
+    else{
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "Please provide a valid Email";
+        text.style.color = "red";
+        document.getElementById("email").style.borderColor = "red";
+    }
+
+
+    
+}
